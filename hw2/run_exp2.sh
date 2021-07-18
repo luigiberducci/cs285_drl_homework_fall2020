ca3f424916ea7d6a07b6414a057f538ff2200ff6
luigiberducci <luigi.berd@gmail.com>
2021-07-18 19:16:09 +0200

    update hw2

--- hw2/run_exp2.sh
+++ hw2/run_exp2.sh
@@ -10,7 +10,7 @@ do
 for s in $seeds
 do
 	echo "launching exp with batch $b and lr=$l"
-	python cs285/scripts/run_hw2.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 1.0 -n 100 -l 2 -s 64 -b ${b} -lr ${l} -rtg --exp_name q2_b${b}_r${l} --seed $s &
+	python cs285/scripts/run_hw2.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 1.0 -n 100 -l 2 -s 64 -b ${b} -lr ${l} -rtg --exp_name q2_b${b}_r${l}_s${s} --seed $s
 done
 done
 done
